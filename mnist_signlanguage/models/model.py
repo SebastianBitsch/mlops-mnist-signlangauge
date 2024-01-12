5d68f792e3f18fd7328d500cec2e20f19b31f408
RaixSplitter <markus.bao.quan@gmail.com>
2024-01-12 14:16:43 +0100

    Better comments and minor change to requirements

--- mnist_signlanguage/models/model.py
+++ mnist_signlanguage/models/model.py
@@ -1,11 +1,19 @@
 import torch.nn as nn
+import torch
 
 class Net(nn.Module):
     """
     Simple CNN network for doing classification on 24 classes
+
+    ARGS: 
+        in_features (int): number of input features
+        n_classes (int): number of classes to predict
+    
+    RETURNS:
+        model (nn.Module): a PyTorch model
     """
 
-    def __init__(self, in_features: int = 1, n_classes: int = 24):
+    def __init__(self, in_features: int = 1, n_classes: int = 24) -> None:
         """
         A small network for doing classification   
         The architecture of the network is adapted from: https://www.kaggle.com/code/lightyagami26/mnist-sign-language-cnn-99-40-accuracy
@@ -32,7 +40,7 @@ class Net(nn.Module):
             nn.Linear(in_features=512, out_features=n_classes)
         )
 
-    def forward(self, x):
+    def forward(self, x) -> torch.Tensor:
         """ Forward pass """
         if (len(x.shape) == 3):
             x = x.unsqueeze(1) # in: [batch_size, 28, 28] -> out: [batch_size, 1, 28, 28]
