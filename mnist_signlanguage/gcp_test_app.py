977f2e12507a2a52de0f6d72f0c42cee5de75e45
RaixSplitter <markus.bao.quan@gmail.com>
2024-01-12 14:28:31 +0100

    API

--- mnist_signlanguage/gcp_test_app.py
+++ mnist_signlanguage/gcp_test_app.py
@@ -3,10 +3,14 @@ from http import HTTPStatus
 from transformers import VisionEncoderDecoderModel, ViTFeatureExtractor, AutoTokenizer
 import torch
 from PIL import Image
-from typing import Dict, List, Any
 
+
+
+
+# Build app
 app = FastAPI()
 
+# Prepare temporary model
 model = VisionEncoderDecoderModel.from_pretrained(
     "nlpconnect/vit-gpt2-image-captioning")
 feature_extractor = ViTFeatureExtractor.from_pretrained(
@@ -14,17 +18,23 @@ feature_extractor = ViTFeatureExtractor.from_pretrained(
 tokenizer = AutoTokenizer.from_pretrained(
     "nlpconnect/vit-gpt2-image-captioning")
 
+# Prepare device
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 model.to(device)
 
+# Prepare kwargs
 gen_kwargs = {"max_length": 16, "num_beams": 8, "num_return_sequences": 1}
 
 
 # Should be replaced with our own models prediction
-def predict_step(images: List[UploadFile]) -> List[str]:
+def predict_step(images: list[UploadFile]) -> list[str]:
     '''
-    Input: Image
-    Output: str
+    Given a list of images, return a list of predictions from a model
+
+    ARGS:
+        images (list[UploadFile]): list of images
+    RETURNS:
+        preds (list[str]): list of predictions
 
     Prediction function taking an image and returning a description of the image based on a transformer from huggingface
     '''
@@ -45,7 +55,7 @@ def predict_step(images: List[UploadFile]) -> List[str]:
 
 
 @app.post("/cv_model/")
-async def cv_model(images: List[Dict[str, Any]] = File(...)) -> Dict[str, Any]:
+async def cv_model(images: list[dict[str, any]] = File(...)) -> dict[str, any]:
     '''
     Input: image in json format
     Output: reponse { "input_images": [image.filename for image in images],
