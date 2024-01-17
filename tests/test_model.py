e9f9c8dcb7ed57fe34852fc7595aa0e752a8579d
Markus <44983702+RaixSplitter@users.noreply.github.com>
2024-01-17 09:24:43 +0100

    Update test_model.py

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,12 +1,12 @@
 import torch
 
-from mnist_signlanguage.models.model import Net 
+from mnist_signlanguage.models.modelTIMM import get_timm 
 
 def test_model():
     in_features = 1
-    n_classes = 24
-    model = Net(in_features = in_features, n_classes = n_classes)
-    x = torch.randn(16, 1, 28, 28)
+    n_classes = 25
+    model = get_timm()
+    x = torch.randn(16, 3, 28, 28)
 
     out = model(x)
     assert out.shape == (16, n_classes)
